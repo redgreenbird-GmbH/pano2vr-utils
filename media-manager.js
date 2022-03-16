c8ac37717d3dd2dcb2c4371314319518dd7c65bc
Adrian Schneider <adrian.schneider@rgbi.ch>
2022-03-16 13:32:47 +0100

    Create media-manager.js

---
+++ media-manager.js
@@ -0,0 +1,15 @@
+function ToggleMediaPlayback(mediaID) {
+  console.log(`ToggleMediaPlayback ${mediaID}`);
+  if (pano.isPlaying(mediaID)) pano.pauseSound(mediaID);
+  else pano.playSound(mediaID);
+}
+function PlayMedia(mediaID) {
+  if (!pano.isPlaying(mediaID)) pano.playSound(mediaID);
+}
+function PlayStopMedia(mediaID) {
+  if (!pano.isPlaying(mediaID)) pano.playSound(mediaID);
+  else pano.stopSound(mediaID);
+}
+function StopMedia(mediaID) {
+  pano.stopSound(mediaID);
+}
