f0bc0452f31e25c06addd88097baf40b355011cc
Adrian Schneider <adrian.schneider@rgbi.ch>
2022-03-17 21:58:20 +0100

    Update media-manager.js

--- media-manager.js
+++ media-manager.js
@@ -3,13 +3,16 @@ function ToggleMediaPlayback(mediaID) {
   if (pano.isPlaying(mediaID)) pano.pauseSound(mediaID);
   else pano.playSound(mediaID);
 }
+
 function PlayMedia(mediaID) {
   if (!pano.isPlaying(mediaID)) pano.playSound(mediaID);
 }
+
 function PlayStopMedia(mediaID) {
   if (!pano.isPlaying(mediaID)) pano.playSound(mediaID);
   else pano.stopSound(mediaID);
 }
+
 function StopMedia(mediaID) {
   pano.stopSound(mediaID);
 }
